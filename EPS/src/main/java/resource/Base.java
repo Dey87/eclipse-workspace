3269c388fabf8195cf474956eab81ce97a0564a9
dey87 <addmin.scanorama@gmail.com>
2020-01-02 08:39:12 +0530

    2nd commit

---
+++ EPS/src/main/java/resource/Base.java
@@ -0,0 +1,50 @@
+package resource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Base {
+	
+	public static WebDriver driver;
+	public Properties prop;	
+public WebDriver initializeDriver() throws IOException
+{
+	
+ prop= new Properties();
+FileInputStream fis=new FileInputStream("C:\\Users\\arka\\EPS\\src\\main\\java\\resource\\data.properties");
+
+prop.load(fis);
+String browserName=prop.getProperty("browser");
+System.out.println(browserName);
+
+if(browserName.equals("chrome"))
+{
+	 System.setProperty("webdriver.chrome.driver", "C:\\Users\\arka\\EPS\\src\\main\\java\\resource\\chromedriver.exe");
+	driver= new ChromeDriver();
+		//execute in chrome driver
+	
+}
+else if (browserName.equals("firefox"))
+{
+	System.setProperty("webdriver.gecko.driver", "C:\\Users\\arka\\EDA6\\src\\main\\java\\resource\\geckodriver.exe");
+	 driver= new FirefoxDriver();
+	//firefox code
+}
+else if (browserName.equals("IE"))
+{
+//	IE code
+}
+
+driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+return driver;
+
+
+}
+
+}
