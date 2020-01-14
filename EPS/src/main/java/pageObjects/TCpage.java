154c57502cd42756ce6479d5a19f0fc45bc248a1
dey87 <addmin.scanorama@gmail.com>
2020-01-14 23:07:29 +0530

    commit14-1-20

--- EPS/src/main/java/pageObjects/TCpage.java
+++ EPS/src/main/java/pageObjects/TCpage.java
@@ -1,5 +1,7 @@
 package pageObjects;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,6 +13,7 @@ public WebDriver driver;
 	
 	private By TemplateGroup=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/select[1]");
 	private By BidParts=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/div[2]/div[1]/select[1]");
+	private By RadioBids=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[1]/div[1]/div[3]/div[1]/div[1]/label[1]/input[1]");
 	private By Tender_Reference_No=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[2]/div[2]/div[1]/input[1]");
 	private By Procurement_Category=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[3]/div[1]/div[1]/select[1]");
 	private By Tender_Currency=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[3]/div[2]/div[1]/select[1]");
@@ -19,7 +22,10 @@ public WebDriver driver;
 	private By Tender_Type=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[5]/div[2]/div[1]/select[1]");
 	private By Minimum_Number_Of_Bids=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[7]/div[1]/div[1]/input[1]");
 	private By Ranking_Sequence=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[8]/div[1]/div[1]/select[1]");
-	private By Detailed_Description=By.xpath("html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[9]/div[1]/div[1]/textarea[1]");
+	private By Detailed_Description=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div/div[1]/div[1]/textarea[1]");
+	private By Submit=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[1]/h3[1]/div[1]/a[1]");
+	private By Tabs=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/ul[1]/li/a[1]");
+	public By ExcelBidding=By.xpath("/html[1]/body[1]/section[2]/section[1]/section[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/form[1]/div[7]/div[1]/div[1]/div[1]/div[1]/button[1]/span[1]");
 	
 	
 	public TCpage(WebDriver driver) {
@@ -36,6 +42,10 @@ public WebDriver driver;
 	public WebElement getBidParts()
 	{
 		return driver.findElement(BidParts);
+	}
+	public WebElement getRadioBids()
+	{
+		return driver.findElement(RadioBids);
 	}
 	public WebElement getTender_Reference_No()
 	{
@@ -72,6 +82,14 @@ public WebDriver driver;
 	public WebElement getDetailed_Description()
 	{
 		return driver.findElement(Detailed_Description);
+	}
+	public WebElement getSubmit()
+	{
+		return driver.findElement(Submit);
+	}
+	public List<WebElement> getTabs()
+	{
+		return driver.findElements(Tabs);
 	}
 
 }
--- EPS/src/test/java/epsTest/NewTenderTest.java
+++ EPS/src/test/java/epsTest/NewTenderTest.java
@@ -1,16 +1,17 @@
 package epsTest;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import pageObjects.LoginPage;
+import pageObjects.TCpage;
 import pageObjects.dashboardPage;
 import resource.Base;
 
@@ -40,9 +41,45 @@ public class NewTenderTest extends Base {
 		dp.getTender_Creation().click();
 		dp.getCreate_New_RFQ().click();
 		
-		Thread.sleep(8000);
+		Thread.sleep(2000);
 		
+		TCpage tp=new TCpage(driver);
 		
+		Select tg=new Select(tp.getTemplateGroup());
+		tg.selectByVisibleText(prop.getProperty("templateG"));
+		
+		Select bp=new Select(tp.getBidParts());
+		bp.selectByVisibleText(prop.getProperty("bidParts"));
+		
+		tp.getRadioBids().click();
+		
+		tp.getTender_Reference_No().sendKeys("hfgjg");
+		
+		Select pc=new Select(tp.getProcurement_Category());
+		pc.selectByVisibleText(prop.getProperty("Procurement_Category"));
+		
+		//tp.getTender_Currency().sendKeys();
+		tp.getPAC().sendKeys("7576778");
+		//tp.getBidding_Currency().sendKeys();
+		
+		Select tt=new Select(tp.getTender_Type());
+		tt.selectByVisibleText(prop.getProperty("Tender_Type"));
+		
+		//tp.getMinimum_Number_Of_Bids().sendKeys("1");
+		
+		//Select rs=new Select(tp.getRanking_Sequence());
+		//rs.selectByVisibleText(prop.getProperty("Ranking_Sequence"));
+		
+		tp.getDetailed_Description().sendKeys("dhgfjfhtfhfhgfhgfhg");
+		
+		tp.getSubmit().click();
+		
+		WebDriverWait wait=new WebDriverWait(driver,20);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(tp.ExcelBidding));
+		List<WebElement> Atabs = tp.getTabs();
+		System.out.println("Number of elements:" +Atabs.size());
+		
+		Thread.sleep(10000);
 		
 		
 		
